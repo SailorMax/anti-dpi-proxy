0cdc40f837c17007c25bdeb851718afeb4edb042
Максим <sailormax@live.com>
2024-08-09 14:10:58 +0300

    update default domains list

--- static/proxy_chooser.js
+++ static/proxy_chooser.js
@@ -2,7 +2,7 @@
 function FindProxyForURL(url, host) {
 
     // use proxy for specific domains
-    if (shExpMatch(host, "youtube.com|*.youtube.com|googlevideo.com|*.googlevideo.com|youtubei.googleapis.com|*.youtubei.googleapis.com"))
+    if (shExpMatch(host, "youtube.com|*.youtube.com|googlevideo.com|*.googlevideo.com|youtubei.googleapis.com|*.youtubei.googleapis.com|ytimg.com|*.ytimg.com"))
         // return "PROXY yourproxy:8080";
 		return "SOCKS 127.0.0.1:1080";
 
